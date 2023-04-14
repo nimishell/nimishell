ec48c6ddfb1d9fce27f6e41d1c2bf1e1ba14f9bd
Yeongseong Go <yeongo@c8r1s4.42seoul.kr>
2023-04-15 08:41:17 +0900

    norm : Fit 25 lines for each functions #9

--- ft_split_token.c
+++ ft_split_token.c
@@ -6,13 +6,11 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 11:56:30 by yeongo            #+#    #+#             */
-/*   Updated: 2023/04/14 23:40:31 by yeongo           ###   ########.fr       */
+/*   Updated: 2023/04/15 08:23:03 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_token.h"
-// #include "ft_memory.h"
-// #include "ft_string.h"
 #include "libft.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -46,14 +44,12 @@ void	token_add_back(t_token **lst, t_token *new)
 	cur->next = new;
 }
 
-int	split_squote(t_token **token, char **read_line)
+int	split_squote(t_token **token, char **read_line, t_token *new_token)
 {
-	t_token	*new_token;
-	char	*line_start;
-	int		line_length;
-	int		count_squote;
+	char *const	line_start = *read_line;
+	int			line_length;
+	int			count_squote;
 
-	line_start = *read_line;
 	line_length = 0;
 	count_squote = 1;
 	(*read_line)++;
@@ -62,33 +58,26 @@ int	split_squote(t_token **token, char **read_line)
 		line_length++;
 		(*read_line)++;
 	}
-	// if (*(*read_line)++ == '\'')
 	if (**read_line == '\'')
 		count_squote++;
 	if (count_squote != 2)
-		return (FAIL);
-	new_token = ft_calloc(1, sizeof(t_token));
-	if (new_token == NULL)
 		return (FAIL);
 	new_token->str = ft_substr(line_start, 1, line_length);
 	if (new_token->str == NULL)
 		return (FAIL);
 	new_token->type = T_CHUNK;
-	// printf("{%s} (%d)\n", new_token->str, new_token->type);
 	token_add_back(token, new_token);
 	if (**read_line != '\0')
 		(*read_line)++;
 	return (SUCCESS);
 }
 
-int	split_dquote(t_token **token, char **read_line)
+int	split_dquote(t_token **token, char **read_line, t_token *new_token)
 {
-	t_token	*new_token;
-	char	*line_start;
-	int		line_length;
-	int		count_dquote;
+	char *const	line_start = *read_line;
+	int			line_length;
+	int			count_dquote;
 
-	line_start = *read_line;
 	line_length = 0;
 	count_dquote = 1;
 	(*read_line)++;
@@ -97,150 +86,138 @@ int	split_dquote(t_token **token, char **read_line)
 		line_length++;
 		(*read_line)++;
 	}
-	// if (*(*read_line)++ == '\"')
 	if (**read_line == '\"')
 		count_dquote++;
 	if (count_dquote != 2)
-		return (FAIL);
-	new_token = ft_calloc(1, sizeof(t_token));
-	if (new_token == NULL)
 		return (FAIL);
 	new_token->str = ft_substr(line_start, 1, line_length);
 	if (new_token->str == NULL)
 		return (FAIL);
 	new_token->type = T_ECHUNK;
-	// printf("{%s} (%d)\n", new_token->str, new_token->type);
 	token_add_back(token, new_token);
 	if (**read_line != '\0')
 		(*read_line)++;
 	return (SUCCESS);
 }
 
-int	split_pipe(t_token **token, char **read_line)
+int	split_pipe(t_token **token, char **read_line, t_token *new_token)
 {
-	t_token	*new_token;
-	char	*line_start;
-	int		line_length;
+	char *const	line_start = *read_line;
+	int			line_length;
 
-	line_start = *read_line;
 	line_length = 0;
 	while (**read_line == '|')
 	{
 		line_length++;
 		(*read_line)++;
 	}
-	new_token = ft_calloc(1, sizeof(t_token));
-	if (new_token == NULL)
-		return (FAIL);
 	new_token->str = ft_substr(line_start, 0, line_length);
 	if (new_token->str == NULL)
 		return (FAIL);
 	new_token->type = T_PIPE;
-	// printf("{%s} (%d)\n", new_token->str, new_token->type);
 	token_add_back(token, new_token);
 	return (SUCCESS);
 }
 
-int	split_redir(t_token **token, char **read_line, char redir_char)
+int	split_redir(t_token **token, char **read_line, \
+		t_token *new_token, char redir_char)
 {
-	t_token	*new_token;
-	char	*line_start;
-	int		line_length;
+	char *const	line_start = *read_line;
+	int			line_length;
 
-	line_start = *read_line;
 	line_length = 0;
 	while (**read_line == redir_char)
 	{
 		line_length++;
 		(*read_line)++;
 	}
-	new_token = ft_calloc(1, sizeof(t_token));
-	if (new_token == NULL)
-		return (FAIL);
 	new_token->str = ft_substr(line_start, 0, line_length);
 	if (new_token->str == NULL)
 		return (FAIL);
 	new_token->type = T_REDIR;
-	// printf("{%s} (%d)\n", new_token->str, new_token->type);
 	token_add_back(token, new_token);
 	return (SUCCESS);
 }
 
-int	split_space(t_token **token, char **read_line)
+int	split_space(t_token **token, char **read_line, t_token *new_token)
 {
-	t_token	*new_token;
-	char	*line_start;
-	int		line_length;
+	char *const	line_start = *read_line;
+	int			line_length;
 
-	line_start = *read_line;
 	line_length = 0;
-	while (**read_line == ' ')
+	while (**read_line == ' ' || **read_line == '\t' || **read_line == '\n')
 	{
 		line_length++;
 		(*read_line)++;
 	}
-	new_token = ft_calloc(1, sizeof(t_token));
-	if (new_token == NULL)
-		return (FAIL);
 	new_token->str = ft_substr(line_start, 0, line_length);
 	if (new_token->str == NULL)
 		return (FAIL);
 	new_token->type = T_SPACE;
-	// printf("{%s} (%d)\n", new_token->str, new_token->type);
 	token_add_back(token, new_token);
 	return (SUCCESS);
 }
 
-int	split_str(t_token **token, char **read_line)
+int	split_str(t_token **token, char **read_line, t_token *new_token)
 {
-	t_token	*new_token;
-	char	*line_start;
-	int		line_length;
+	char *const	line_start = *read_line;
+	int			line_length;
 
-	line_start = *read_line;
 	line_length = 0;
 	while (**read_line && !ft_issep(**read_line, "\'\"|<> "))
 	{
 		line_length++;
 		(*read_line)++;
 	}
-	new_token = ft_calloc(1, sizeof(t_token));
-	if (new_token == NULL)
-		return (FAIL);
 	new_token->str = ft_substr(line_start, 0, line_length);
 	if (new_token->str == NULL)
 		return (FAIL);
 	new_token->type = T_ECHUNK;
-	// printf("{%s} (%d)\n", new_token->str, new_token->type);
 	token_add_back(token, new_token);
 	return (SUCCESS);
 }
 
 int	ft_split_token(t_token **token_head, char *read_line)
 {
-	int	result;
+	t_token	*new_token;
+	int		result;
 
 	while (*read_line)
 	{
-		// printf("start_char : {%c}\n", *read_line);
+		new_token = ft_calloc(1, sizeof(t_token));
+		if (new_token == NULL)
+			return (FAIL);
 		if (*read_line == '\'')
-			result = split_squote(token_head, &read_line);
+			result = split_squote(token_head, &read_line, new_token);
 		else if (*read_line == '\"')
-			result = split_dquote(token_head, &read_line);
+			result = split_dquote(token_head, &read_line, new_token);
 		else if (*read_line == '|')
-			result = split_pipe(token_head, &read_line);
-		else if (*read_line == '<')
-			result = split_redir(token_head, &read_line, '<');
-		else if (*read_line == '>')
-			result = split_redir(token_head, &read_line, '>');
-		else if (*read_line == ' ')
-			result = split_space(token_head, &read_line);
+			result = split_pipe(token_head, &read_line, new_token);
+		else if (*read_line == '<' || *read_line == '>')
+			result = split_redir(token_head, &read_line, new_token, *read_line);
+		else if (*read_line == ' ' || *read_line == '\t' || *read_line == '\n')
+			result = split_space(token_head, &read_line, new_token);
 		else
-			result = split_str(token_head, &read_line);
+			result = split_str(token_head, &read_line, new_token);
 		if (result == FAIL)
-			return (0);
+			return (FAIL);
 	}
-	return (1);
+	return (SUCCESS);
+}
+
+void	token_clear(t_token **token)
+{
+	t_token	*remove_token;
+
+	if (*token == NULL)
+		return ;
+	while (*token != NULL)
+	{
+		remove_token = *token;
+		*token = (*token)->next;
+		free(remove_token->str);
+		free(remove_token);
+	}
 }
 
 void	print_tokens(t_token *token)
@@ -248,10 +225,9 @@ void	print_tokens(t_token *token)
 	t_token	*cur;
 
 	cur = token;
-	printf("\n");
 	while (cur != NULL)
 	{
-		printf("{%s} (%d)\n", cur->str, cur->type);
+		printf("token : {%s} (%d)\n", cur->str, cur->type);
 		cur = cur->next;
 	}
 }
@@ -266,9 +242,12 @@ int	main(int ac, char **av)
 	read_line = ft_strtrim(av[1], " ");
 	if (read_line == NULL)
 		return (1);
-	printf("read_line : {%s}\n", read_line);
-	if (!ft_split_token(&token, read_line))
+	printf("read_line : {%s}\n\n", read_line);
+	if (ft_split_token(&token, read_line) == FAIL)
+	{
+		token_clear(&token);
 		return (1);
+	}
 	print_tokens(token);
 	return (0);
 }
--- tokenize.sh
+++ tokenize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cc ft_split_token.c libft/srcs/ft_calloc.c libft/srcs/ft_strtrim.c libft/srcs/ft_substr.c libft/srcs/ft_memset.c libft/srcs/ft_strlen.c libft/srcs/ft_bzero.c libft/srcs/ft_strchr.c libft/srcs/ft_strdup.c libft/srcs/ft_strlcpy.c -I./libft/inc;
-./a.out "   gndk     | kdjkf ||||| <<<<< >>>> \"gn'd'kfj\" '.....'  "
+./a.out "   gndk   	| kdjkf ||||| <<<<< >>>> | | | | | \"gn'd'kfj\" '.....'\"ndkfdfk\"  "
