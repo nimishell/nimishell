50e29d28dd1aa56aa5e7dcfe7efd7d8492531e63
Yeongseong Go <yeongo@c9r3s4.42seoul.kr>
2023-04-20 22:05:01 +0900

    refactor: delete some function for error #11

--- srcs/error.c
+++ srcs/error.c
@@ -6,62 +6,64 @@
 /*   By: wbae <wbae@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:37:37 by wbae              #+#    #+#             */
-/*   Updated: 2023/04/20 21:03:16 by wbae             ###   ########.fr       */
+/*   Updated: 2023/04/20 22:18:58 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <sys/errno.h>
 
-extern int	errno;
-
-void	ft_perror(char *p_name, char *argument, int code)
+void	ft_perror(char *cmd, char *arg)
 {
-	char	*str;
+	char	*err_message;
 
-	str = strerror(errno);
+	err_message = strerror(errno);
 	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(p_name, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(argument, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(str, 2);
+	if (cmd != NULL)
+	{
+		ft_putstr_fd(cmd, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	if (arg != NULL)
+	{
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	ft_putstr_fd(err_message, 2);
 	ft_putstr_fd("\n", 2);
-	exit(code);
 }
 
-void	print_error(char *str, int code)
+void	exit_with_perror(char *cmd, char *arg, int status)
+{
+	ft_perror(cmd, arg);
+	exit(status);
+}
+
+void	exit_with_message(char *cmd, char *arg, char *messsage, int status)
 {
 	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(str, 2);
+	if (cmd != NULL)
+	{
+		ft_putstr_fd(cmd, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	if (arg != NULL)
+	{
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	ft_putstr_fd(messsage, 2);
 	ft_putstr_fd("\n", 2);
-	exit(code);
+	exit(status);
 }
 
-void	cmd_error(char *str, int code)
-{
-	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(str, 2);
-	exit(code);
-}
-
-void	print_cmd_error(char *str, char *cmd, int code)
-{
-	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(str, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(cmd, 2);
-	ft_putstr_fd("\n", 2);
-	exit(code);
-}
-
-void	syntax_error(char *str, int exit_code, int flag)
+void	syntax_error(char *message, int status, int flag)
 {
 	ft_putstr_fd("minishell: syntax error", 2);
-	ft_putstr_fd(str, 2);
+	ft_putstr_fd(message, 2);
 	ft_putstr_fd("\n", 2);
 	if (!flag)
-		g_env->status = exit_code;
+		g_env->status = status;
 	if (flag)
-		exit(exit_code);
+		exit(status);
 }
